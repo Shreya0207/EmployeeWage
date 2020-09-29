9b566dd3af2bb533e545c7d97291faef5aa7c982
Shreya0207 <shanu.shreya270@gmail.com>
2020-09-29 20:13:42 +0530

    [Shreya]Refactor: Checking employee attendance

--- EmployeeWageProgram/Program.cs
+++ EmployeeWageProgram/Program.cs
@@ -7,6 +7,13 @@ namespace EmployeeWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage calculation program");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
