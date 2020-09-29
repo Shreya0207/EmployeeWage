698f340f3c92a75766d8aa312d58f0eb7040de02
Shreya0207 <shanu.shreya270@gmail.com>
2020-09-29 20:28:57 +0530

    [Shreya]Refactor: Solving using switch case statement

--- EmployeeWageProgram/Program.cs
+++ EmployeeWageProgram/Program.cs
@@ -4,11 +4,11 @@ namespace EmployeeWageProgram
 {
     class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Employee Wage calculation program");
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
+            Console.WriteLine("Welcome to Employee Wage Computation.");
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
@@ -18,12 +18,18 @@ namespace EmployeeWageProgram
                 Console.WriteLine("Employee is Present");
             else
                 Console.WriteLine("Employee is absent");
-            if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-            else if (empCheck == IS_FULL_TIME)
-                empHrs = 8;
-            else
-                empHrs = 0;
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage: " + empWage);
         }
