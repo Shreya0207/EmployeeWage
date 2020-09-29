8206bf4b88891c7240e85dcbd91a8e538881e86c
Shreya0207 <shanu.shreya270@gmail.com>
2020-09-29 20:09:08 +0530

    [Shreya]Add: Displaying the welcome message

---
+++ EmployeeWageProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage calculation program");
+        }
+    }
+}
